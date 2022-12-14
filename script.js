ee420859dd57d9b2ed0d4e4640880607f33af8b8
blisecki <brayden.lisecki@gmail.com>
2022-12-14 09:21:11 -0700

    Generated the 4x4 grid, added a title

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const container = document.querySelector('.container');
+const rows = document.querySelectorAll('.row');
 
-for(i=1; i < 17; i++) {
-    container.appendChild(document.createElement('div'));
+for(i=0; i < 4; i++) {
+    let row = container.appendChild(document.createElement('div'));
+    row.classList.add('row');
+    for(x=0; x < 4; x++) {
+        let box = row.appendChild(document.createElement('div'));
+        box.classList.add('box');
+    }
 }
-
-const boxes = document.querySelectorAll('div');
-boxes.forEach((box) => {
-    box.classList.add('box');
-
-  });
-
-container.classList.remove('box');
