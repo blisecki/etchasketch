48b4f5b08613c84394b71c4041ee48a2e74e402a
blisecki <brayden.lisecki@gmail.com>
2022-12-14 21:51:40 -0700

    added button to take input for grid size and generate grid, final commit for project for now

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const container = document.querySelector('.container');
 const rows = document.querySelectorAll('.row');
+let x=0;
 
-for(i=0; i < 4; i++) {
-    let row = container.appendChild(document.createElement('div'));
-    row.classList.add('row');
-    for(x=0; x < 4; x++) {
-        let box = row.appendChild(document.createElement('div'));
-        box.classList.add('box');
+
+function showAlert() {
+    x = prompt("Enter grid size:");
+    console.log(x);
+    if(x > 100 || x < 1 || x =="") {
+        
     }
+    for(i=0; i < x; i++) {
+        let row = container.appendChild(document.createElement('div'));
+        row.classList.add('row');
+        for(z=0; z < x; z++) {
+            let box = row.appendChild(document.createElement('div'));
+            box.classList.add('box');
+        }
+    }
+
+    const cells = document.querySelectorAll('.box');
+
+    cells.forEach((cell) => {
+    
+      cell.addEventListener('mouseover', () => {
+        cell.classList.add('active');
+      });
+    });
+
 }
